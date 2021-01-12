85cb86c6bcdac00bf7aee8ea4952cbf69594b407
sapreashu@gmail.com <himanshu.gandhi.knoxpo@gmail.com>
2021-01-12 11:43:51 +0530

    Initial Commit

---
+++ FileUploadInFireStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FileUploadInFireStore
+//
+//  Created by Knoxpo MacBook Pro on 12/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
